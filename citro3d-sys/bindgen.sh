a7690e7cdde7f35cfce69a056ea246136c2e93d4
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2022-11-21 21:56:50 -0500

    Only generate citro3d-specific types/funcs, etc.

--- citro3d-sys/bindgen.sh
+++ citro3d-sys/bindgen.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-bindgen "$DEVKITPRO/libctru/include/citro3d.h" \
+bindgen "$DEVKITPRO/libctru/include/tex3ds.h" \
     --rust-target nightly \
     --use-core \
     --distrust-clang-mangling \
@@ -10,26 +10,15 @@ bindgen "$DEVKITPRO/libctru/include/citro3d.h" \
     --ctypes-prefix "::libc" \
     --no-prepend-enum-name \
     --fit-macro-constant-types \
+    --raw-line "use ctru_sys::*;" \
     --must-use-type "Result" \
     --generate "functions,types,vars" \
     --blocklist-type "u(8|16|32|64)" \
     --opaque-type "GPU_.*" \
     --opaque-type "GFX_.*" \
     --opaque-type "float24Uniform_s" \
-    --allowlist-type "C3D_.*" \
-    --allowlist-type "DVLB_.*" \
-    --allowlist-type "shader.*" \
-    --allowlist-type "float24Uniform_s" \
-    --allowlist-function "C3D_.*" \
-    --allowlist-function "shader.*" \
-    --allowlist-function "DVLB_.*" \
-    --allowlist-function "linear.*" \
-    --allowlist-var "C3D_.*" \
-    --allowlist-type "GPU_.*" \
-    --allowlist-type "GX_.*" \
-    --allowlist-function 'AttrInfo_(Init|AddLoader|AddFixed)' \
-    --allowlist-function 'BufInfo_(Init|Add)' \
-    --allowlist-function 'Mtx_.*' \
+    --allowlist-file ".*/c3d/.*[.]h" \
+    --allowlist-file ".*/tex3ds[.]h" \
     -- \
     --target=arm-none-eabi \
     --sysroot=$DEVKITARM/arm-none-eabi \
