085ab595ec9f23c178b726c61d0ed0c8b229820f
Natasha England-Elbro <natasha@natashaee.me>
2024-02-15 12:35:06 +0000

    fix: red and blue channels swapped in material rendering

--- citro3d/src/material.rs
+++ citro3d/src/material.rs
@@ -9,15 +9,16 @@ pub struct Material {
 impl Material {
     pub fn to_raw(self) -> citro3d_sys::C3D_Material {
         citro3d_sys::C3D_Material {
-            ambient: self.ambient.unwrap_or_default().to_parts(),
-            diffuse: self.diffuse.unwrap_or_default().to_parts(),
-            specular0: self.specular0.unwrap_or_default().to_parts(),
-            specular1: self.specular1.unwrap_or_default().to_parts(),
-            emission: self.emission.unwrap_or_default().to_parts(),
+            ambient: self.ambient.unwrap_or_default().to_parts_bgr(),
+            diffuse: self.diffuse.unwrap_or_default().to_parts_bgr(),
+            specular0: self.specular0.unwrap_or_default().to_parts_bgr(),
+            specular1: self.specular1.unwrap_or_default().to_parts_bgr(),
+            emission: self.emission.unwrap_or_default().to_parts_bgr(),
         }
     }
 }
 
+/// RGB color in linear space ([0, 1])
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Color {
     pub r: f32,
@@ -29,7 +30,12 @@ impl Color {
     pub fn new(r: f32, g: f32, b: f32) -> Self {
         Self { r, g, b }
     }
-    pub fn to_parts(self) -> [f32; 3] {
-        [self.r, self.g, self.b]
+    /// Split into BGR ordered parts
+    ///
+    /// # Reason for existence
+    /// The C version of [`Material`] expects colours in BGR order (don't ask why it is beyond my comprehension)
+    /// so we have to reorder when converting
+    pub fn to_parts_bgr(self) -> [f32; 3] {
+        [self.b, self.g, self.r]
     }
 }
